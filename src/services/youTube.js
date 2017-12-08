1e0210c1715160118a550f5360ccc78e39a14c35
jdbyrd <jd3yrd@gmail.com>
2017-12-08 17:26:54 -0600

    add service function

--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,22 @@
 angular.module('video-player')
-.service('youTube', function(){
-  // TODO
-});
+  .service('youTube', function($http) {
+    this.getVideos = function(query) {
+      options = {
+        q: query,
+        maxResults: 5,
+        videoEmbeddable: 'true',
+        type: 'video',
+        part: 'snippet',
+        key: window.YOUTUBE_API_KEY
+      };
+      $http({
+        method: 'GET',
+        url: 'https://www.googleapis.com/youtube/v3/search',
+        data: options
+      }).then(function successCallback(response) {
+        console.log('success');
+      }, function errorCallback(response) {
+        console.log('error');
+      });
+    };
+  });
