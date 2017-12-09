d482b88afcc3febd21a7c7fc023f242475624ad3
jdbyrd <jd3yrd@gmail.com>
2017-12-08 19:41:18 -0600

    Fetching data from Youtube API

--- src/components/app.js
+++ src/components/app.js
@@ -2,13 +2,19 @@ angular.module('video-player')
 
   .component('app', { 
     templateUrl: 'src/templates/app.html',
-    controller: function() {
+    controller: function(youTube) {
+      
       this.videos = window.exampleVideoData;
       this.currentVideo = this.videos[0];
-      
+      this.search = function(results) {
+        this.videos = results;
+        this.currentVideo = results[0];
+      };
       this.click = function(clickedVideo) {
         this.currentVideo = clickedVideo;
       };
       this.click = this.click.bind(this);
+      this.search = this.search.bind(this);
+      youTube.getVideos('rick astley', this.search);
     }
   });
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,22 +1,29 @@
 angular.module('video-player')
   .service('youTube', function($http) {
-    this.getVideos = function(query) {
+    this.getVideos = function(query, callback) {
       options = {
         q: query,
+        part: 'snippet',
         maxResults: 5,
         videoEmbeddable: 'true',
         type: 'video',
+        key: window.YOUTUBE_API_KEY
+      };
+      params = {
         part: 'snippet',
         key: window.YOUTUBE_API_KEY
       };
       $http({
         method: 'GET',
         url: 'https://www.googleapis.com/youtube/v3/search',
-        data: options
+        params: options,
+        //data: options
       }).then(function successCallback(response) {
         console.log('success');
+        console.log('success', response.data.items);
+        callback(response.data.items);
       }, function errorCallback(response) {
-        console.log('error');
+        console.log('error', response);
       });
     };
   });
