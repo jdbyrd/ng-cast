03e34f65914d13fc48d7628f795fd88d79ca58ee
jdbyrd <jd3yrd@gmail.com>
2017-12-09 10:18:29 -0600

    App fully functional with live search

--- src/components/app.js
+++ src/components/app.js
@@ -6,15 +6,24 @@ angular.module('video-player')
       
       this.videos = window.exampleVideoData;
       this.currentVideo = this.videos[0];
+      
+      this.searchApi = function(input) {
+        youTube.getVideos(input, this.search);
+      };
+
       this.search = function(results) {
         this.videos = results;
         this.currentVideo = results[0];
       };
+      
       this.click = function(clickedVideo) {
         this.currentVideo = clickedVideo;
       };
+      
       this.click = this.click.bind(this);
       this.search = this.search.bind(this);
+      this.searchApi = this.searchApi.bind(this);
+      
       youTube.getVideos('rick roll', this.search);
     }
   });
--- src/components/search.js
+++ src/components/search.js
@@ -2,6 +2,9 @@ angular.module('video-player')
 
   .component('search', {
     templateUrl: 'src/templates/search.html',
+    bindings: {
+      searchApi: '<'
+    },
     controller: function() {
       this.input;
     }
